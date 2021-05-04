6c03ea7be1dcfe585b5177f54ba8bd0fa88c570a
Smart Precast NB2 <smartprecast.nb2@gmail.com>
2021-05-04 03:22:01 +0000

    now uses dedicated pattern and added toJSon

--- lib/sql/sintax parser/orderByStatement.dart
+++ lib/sql/sintax parser/orderByStatement.dart
@@ -7,11 +7,15 @@ class OrderByStatement {
   List<OrderByStatementUnit> statements = [];
 
   OrderByStatement (String statement) {
-    var orderByRegExp = RegExp(orderByUnitPat, caseSensitive: false);
+    var orderByRegExp = RegExp('(?<orderByColumn>$singleVarPat)(\\s+(?<orderBySort>desc|asc))', caseSensitive: false);
     var matches = orderByRegExp.allMatches(statement);
     for (var match in matches) {
       statements.add(OrderByStatementUnit(match));
     }
+  }
+
+  List<Map<String, dynamic>> toJson() {
+    return statements.map((e) => e.toJson()).toList();
   }
 
 }
