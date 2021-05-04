2f5ace9e19e128e0508c4047e230a716106d4ebd
Smart Precast NB2 <smartprecast.nb2@gmail.com>
2021-05-04 03:20:16 +0000

    increased error reporting and added toJson

--- lib/sql/sintax parser/whereStatementUnit.dart
+++ lib/sql/sintax parser/whereStatementUnit.dart
@@ -7,11 +7,24 @@ class WhereStatementUnit {
   String value;
 
   WhereStatementUnit (String s) {
-    var whereComparatorRegExp = RegExp(whereComparatorPat);
+    var v = '(?<whereColumn>$singleVarPat)\\s+(?<whereOperator>$whereOperatorPat)\\s+(?<whereValue>$numberPat|$stringValuePat|$singleVarPat)';
+    var whereComparatorRegExp = RegExp(v, caseSensitive: false);
     var match = whereComparatorRegExp.firstMatch(s);
+    if (match == null) {
+      
+      throw Exception('Tried to access groups of non where unit: $s.\nPattern: $v');
+    }
     column = match.namedGroup('whereColumn');
     op = match.namedGroup('whereOperator');
     value = match.namedGroup('whereValue');
+  }
+
+  Map<String, dynamic> toJson () {
+    return {
+      'column': column,
+      'operator': op,
+      'value': value
+    };
   }
 
 }
