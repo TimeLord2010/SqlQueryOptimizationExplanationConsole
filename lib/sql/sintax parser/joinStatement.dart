8675e2669e7969e9cdae59efd19d60d1dd12b4f0
Smart Precast NB2 <smartprecast.nb2@gmail.com>
2021-05-04 03:22:42 +0000

    now uses dedicated pattern and added toJson

--- lib/sql/sintax parser/joinStatement.dart
+++ lib/sql/sintax parser/joinStatement.dart
@@ -7,11 +7,15 @@ class JoinStatement {
   List<JoinStatementUnit> statement = [];
 
   JoinStatement (String s) {
-    var singleJoinRegExp = RegExp(singleJoinPat, caseSensitive: false);
+    var singleJoinRegExp = RegExp('join\\s+(?<joinTable>$singleVarPat)\\s+on\\s+(?<joinCondition>$whereExpressionPat)', caseSensitive: false);
     var matches = singleJoinRegExp.allMatches(s);
     for (var match in matches) {
       statement.add(JoinStatementUnit(match));
     }
+  }
+
+  List<Map<String, dynamic>> toJson() {
+    return statement.map((e) => e.toJson()).toList();
   }
 
 }
