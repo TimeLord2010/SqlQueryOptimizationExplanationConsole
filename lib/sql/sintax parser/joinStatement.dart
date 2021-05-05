755e29ea692121e46599e20fbfc55d1e103123f8
Smart Precast NB2 <smartprecast.nb2@gmail.com>
2021-05-05 04:07:10 +0000

    Added getTables and getColumns methods

--- lib/sql/sintax parser/joinStatement.dart
+++ lib/sql/sintax parser/joinStatement.dart
@@ -6,16 +6,34 @@ class JoinStatement {
 
   List<JoinStatementUnit> statement = [];
 
-  JoinStatement (String s) {
-    var singleJoinRegExp = RegExp('join\\s+(?<joinTable>$singleVarPat)\\s+on\\s+(?<joinCondition>$whereExpressionPat)', caseSensitive: false);
+  JoinStatement(String s) {
+    var singleJoinRegExp = RegExp(
+        'join\\s+(?<joinTable>$singleVarPat)\\s+on\\s+(?<joinCondition>$whereExpressionPat)',
+        caseSensitive: false);
     var matches = singleJoinRegExp.allMatches(s);
     for (var match in matches) {
       statement.add(JoinStatementUnit(match));
     }
   }
 
+  Set<String> getTables() {
+    var tables = <String>{};
+    for (var joinUnit in statement) {
+      tables.add(joinUnit.joinedTable);
+      tables.addAll(joinUnit.where.getTables());
+    }
+    return tables;
+  }
+
+  Set<String> getColumns () {
+    var columns = <String>{};
+    for (var unit in statement) {
+      columns.addAll(unit.where.getColumns());
+    }
+    return columns;
+  }
+
   List<Map<String, dynamic>> toJson() {
     return statement.map((e) => e.toJson()).toList();
   }
-
 }
