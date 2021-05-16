0e6824adde68655644426b07d7bccd657fb3f636
Smart Precast NB2 <smartprecast.nb2@gmail.com>
2021-05-16 06:44:16 +0000

    added natural join

---
+++ lib/relationalAlgebra/operations/naturalJoin.dart
@@ -0,0 +1,16 @@
+import 'package:console/relationalAlgebra/util/RAoperator.dart';
+import 'package:console/relationalAlgebra/util/dataSet.dart';
+
+class NaturalJoin implements RAoperator {
+  @override
+  var source;
+
+  @override
+  String symbol = '⋈';
+
+  @override
+  DataSet process() {
+    // TODO: implement process
+    throw UnimplementedError();
+  }
+}
