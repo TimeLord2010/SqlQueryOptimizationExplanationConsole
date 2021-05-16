997556c58468039176569b2bb41eac595d1b2d2f
Smart Precast NB2 <smartprecast.nb2@gmail.com>
2021-05-16 06:44:27 +0000

    added cross product

---
+++ lib/relationalAlgebra/operations/crossProduct.dart
@@ -0,0 +1,16 @@
+import 'package:console/relationalAlgebra/util/RAoperator.dart';
+import 'package:console/relationalAlgebra/util/dataSet.dart';
+
+class CrossProduct implements RAoperator {
+  @override
+  var source;
+
+  @override
+  String symbol = '×';
+
+  @override
+  DataSet process() {
+    // TODO: implement process
+    throw UnimplementedError();
+  }
+}
