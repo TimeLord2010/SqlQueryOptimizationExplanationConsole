5421c12aed15af861d2422881e7b5b9763e5d1df
Smart Precast NB2 <smartprecast.nb2@gmail.com>
2021-05-17 23:37:35 +0000

    added named constructor fromUnits

--- lib/relationalAlgebra/util/conditional.dart
+++ lib/relationalAlgebra/util/conditional.dart
@@ -3,26 +3,55 @@ import 'package:console/sql/sintax%20parser/whereStatement.dart';
 import 'package:console/sql/sintax%20parser/whereStatementUnit.dart';
 
 class Conditional {
+  Conditional parent;
   dynamic left;
   bool and;
   dynamic right;
 
   Conditional();
 
-  static Conditional fromWhereStatement(WhereStatement st, String defaultTable) {
+  Conditional.fromUnits(Iterable<ConditionalUnit> units, {Conditional p}) {
+    if (units.isEmpty) {
+      throw Exception('Cannot build conditional with no units.');
+    }
+    parent = p;
+    left = units.first;
+    if (units.length == 2) {
+      and = true;
+      right = units.elementAt(1);
+    } else {
+      and = true;
+      right = Conditional.fromUnits(units.skip(1), p: this);
+    }
+  }
+
+  static void loop(Conditional conditional, Function(ConditionalUnit) func) {
+    if (conditional.left is Conditional) {
+      loop(conditional.left, func);
+    } else if (conditional.left is ConditionalUnit) {
+      func(conditional.left);
+    }
+    if (conditional.right is Conditional) {
+      loop(conditional.right, func);
+    } else if (conditional.right is ConditionalUnit) {
+      func(conditional.right);
+    }
+  }
+
+  static Conditional fromWhereStatement(WhereStatement st, String defaultTable, {Conditional parent}) {
     if (st == null) return null;
     var c = Conditional();
     if (st.left is WhereStatement) {
-      c.left = Conditional.fromWhereStatement(st.left, defaultTable);
+      c.left = Conditional.fromWhereStatement(st.left, defaultTable, parent: c);
     } else if (st.left is WhereStatementUnit) {
-      c.left = ConditionalUnit.fromWhereUnit(st.left, defaultTable);
+      c.left = ConditionalUnit.fromWhereUnit(st.left, defaultTable, p: c);
     }
     if (st.right is WhereStatement) {
       c.and = st.and;
-      c.right = Conditional.fromWhereStatement(st.right, defaultTable);
+      c.right = Conditional.fromWhereStatement(st.right, defaultTable, parent: c);
     } else if (st.right is WhereStatementUnit) {
       c.and = st.and;
-      c.right = ConditionalUnit.fromWhereUnit(st.right, defaultTable);
+      c.right = ConditionalUnit.fromWhereUnit(st.right, defaultTable, p: c);
     }
     return c;
   }
@@ -33,7 +62,7 @@ class Conditional {
       var sep = and ? '˄' : '˅';
       return '$left $sep $right';
     } else {
-		return left.toString();
-	}
+      return left.toString();
+    }
   }
 }
