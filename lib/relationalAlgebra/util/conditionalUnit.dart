a11dfeba88862943e8e57f40cc745580e38291b3
Smart Precast NB2 <smartprecast.nb2@gmail.com>
2021-05-17 23:37:04 +0000

    added parent

--- lib/relationalAlgebra/util/conditionalUnit.dart
+++ lib/relationalAlgebra/util/conditionalUnit.dart
@@ -1,14 +1,18 @@
 import 'package:console/relationalAlgebra/util/column.dart';
+import 'package:console/relationalAlgebra/util/conditional.dart';
 import 'package:console/sql/sintax%20parser/whereStatementUnit.dart';
 
 class ConditionalUnit {
+
+  Conditional parent;
   Column column;
   String op;
   String value;
 
   ConditionalUnit();
 
-  ConditionalUnit.fromWhereUnit(WhereStatementUnit unit, String defaultTable) {
+  ConditionalUnit.fromWhereUnit(WhereStatementUnit unit, String defaultTable, {Conditional p}) {
+    parent = p;
     if (unit.column.contains('.')) {
       var parts = unit.column.split('.');
       column = Column(parts[0], parts[1]);
