6cc6c4b937433903226e483667ad0d6b13d795f5
Smart Precast NB2 <smartprecast.nb2@gmail.com>
2021-05-20 01:07:53 +0000

    finished query optimizer

--- lib/queryOptimizer/queryOptimizer.dart
+++ lib/queryOptimizer/queryOptimizer.dart
@@ -1,4 +1,6 @@
+import 'package:console/relationalAlgebra/operations/projection.dart';
 import 'package:console/relationalAlgebra/operations/selection.dart';
+import 'package:console/relationalAlgebra/util/column.dart';
 import 'package:console/relationalAlgebra/util/conditional.dart';
 import 'package:console/relationalAlgebra/util/conditionalUnit.dart';
 import 'package:console/relationalAlgebra/util/navigator.dart';
@@ -6,7 +8,7 @@ import 'package:console/relationalAlgebra/util/navigator.dart';
 class SelectionAndConditional {
   Selection selection;
   Conditional conditional;
-  SelectionAndConditional (Selection s, Conditional c) {
+  SelectionAndConditional(Selection s, Conditional c) {
     selection = s;
     conditional = c;
   }
@@ -19,25 +21,18 @@ void setConditionalsToEspecificSelections(RAnavigator nav, List<String> tables) 
       selections.add(op);
     }
   });
-  //print('tables: $tables');
   for (var table in tables) {
-    //print('table: $table');
     var op = RAnavigator.lookFor(nav: nav, table: table);
     if (op == null) throw Exception('Cannot find operator with source set to $table.');
     var cond = <ConditionalUnit>[];
     for (var selection in selections) {
-      //print('conditional loop: ');
       Conditional.loop(selection.condition, (c) {
-        //print('c: $c');
-        //print('table: ${c.column.table} == $table => ${c.column.table == table}');
         if (c.column.table == table) {
-          //print('added');
           cond.add(c);
         }
       });
     }
     var selection = Selection();
-    //print('conditionals empty? ${cond.length}');
     selection.condition = Conditional.fromUnits(cond);
     selection.source = table;
     if (op.source is String && op.source == table) {
@@ -47,30 +42,52 @@ void setConditionalsToEspecificSelections(RAnavigator nav, List<String> tables) 
           nav.remove(selection);
         }
       }
-      //print('overring source: ${op.source}. In expression: $op. For: $selection\n');
       op.source = selection;
     } else if (['⋈θ', '×', '⋈'].contains(op.symbol) && op.source2 is String && op.source2 == table) {
-      //print('overring source: ${op.source2}. In expression: $op. For: $selection\n');
       op.source2 = selection;
     } else {
       print('tried overring source: ${op.source}. In expression: $op. For: $selection\n');
     }
   }
-  // if (nav.expression is Projection) {
-  //   nav.goDown();
-  //   setConditionalsToEspecificSelections(nav, conditionals);
-  // }
-  // if (nav.expression is Selection) {
-  //   var exp = nav.expression as Selection;
-  //   conditionals.add(exp.condition);
-  //   nav.goDown();
-  //   setConditionalsToEspecificSelections(nav, conditionals);
-  // }
+}
+
+void setProjectionsToEspecificSelections(RAnavigator nav, List<String> tables) {
+  nav.goTop();
+  var allColumns = <Column>[];
+  if (nav.expression is Projection) {
+    var projection = nav.expression as Projection;
+    projection.outputColumns.forEach((element) {
+      allColumns.add(element);
+    });
+  } else {
+    throw Exception('Top operator should be projection. found: ${nav.expression}');
+  }
+  for (var table in tables) {
+    var op = RAnavigator.lookFor(nav: nav, table: table);
+    if (op == null) throw Exception('Cannot find operator with source set to $table.');
+    if (!(op is Selection)) throw Exception('Operator should be a selection.');
+    var opParent = nav.parentOf(op);
+    if (opParent == null) throw Exception('Cannot find parent of $op');
+    var columns = allColumns.where((element) => element.table == table);
+    var projection = Projection.fromColumns(columns.toList(), op);
+    if (['⋈θ', '×', '⋈'].contains(opParent.symbol)) {
+      if (opParent.source == op) {
+        opParent.source = projection;
+      } else if (opParent.source2 == op) {
+        opParent.source2 = projection;
+      } else {
+        throw Exception('No branch was the original item.');
+      }
+    } else {
+      opParent.source = projection;
+    }
+  }
 }
 
 dynamic optimize({op, List<String> tables}) {
   var nav = RAnavigator(op);
   setConditionalsToEspecificSelections(nav, tables);
+  setProjectionsToEspecificSelections(nav, tables);
   nav.goTop();
   return nav.expression;
 }
